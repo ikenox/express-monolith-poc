6db974df8a54fa770f13e1c7b831807a41c81668
naoto.ikeno <ikenox@gmail.com>
2023-09-23 14:50:29 +0900

    serve two express apps

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,21 @@ import express from 'express';
 import payload from 'payload';
 
 require('dotenv').config();
-const app = express();
-
-// Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin');
-});
 
 const start = async () => {
-  // Initialize Payload
+  const defaultApp = express();
+  defaultApp.listen(3000);
+
+  const payloadApp = express();
   await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     mongoURL: process.env.MONGODB_URI,
-    express: app,
+    express: payloadApp,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
-
-  // Add your own express routes here
-
-  app.listen(3000);
-}
+  });
+  payloadApp.listen(3001);
+};
 
 start();
