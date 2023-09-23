7b94e18deef9e67acac6d3440b6a6cf518cc5123
naoto.ikeno <ikenox@gmail.com>
2023-09-24 00:02:10 +0900

    update

--- src/server1/server.ts
+++ src/server1/server.ts
@@ -36,7 +36,7 @@ const typeDefs = gql`
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    books: [Book] @cacheControl(maxAge: 0)
   }
 `
 
