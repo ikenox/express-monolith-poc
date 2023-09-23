898fa59928b32cf3db141e6f90a6284e6aa1e137
naoto.ikeno <ikenox@gmail.com>
2023-09-24 00:26:09 +0900

    update

--- src/payloadcms/collections/Articles.ts
+++ src/payloadcms/collections/Articles.ts
@@ -2,9 +2,6 @@ import { CollectionConfig } from 'payload/types';
 
 const Articles: CollectionConfig = {
   slug: 'articles',
-  auth: {
-    disableLocalStrategy: true,
-  },
   fields: [
     {
       name: 'title',
